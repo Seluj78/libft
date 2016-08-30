b9157d1ce7cca783c80ce7899819dbe9ec3d36df
jlasne <jlasne@student.42.fr>
2016-08-30 10:49:26 +0200

    finished the strtrim function, NOT TESTED

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jlasne <jlasne@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/30 01:17:51 by jlasne            #+#    #+#             */
-/*   Updated: 2016/08/30 01:34:15 by jlasne           ###   ########.fr       */
+/*   Updated: 2016/08/30 10:48:38 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,24 @@ char	*ft_strjoin(char const *s)
 
 	j = 0;
 	i = 0;
+	index_end = 0;
 	size_s = ft_strlen(s);
 	if (!(ret = (char *) malloc((size_s + 1) * sizeof (char))))
 			return (NULL);
 	while (s[i] != ' ' || s[i] != '\t' || s[i] != '\n')
 		i++;
+	while (s[size_s] == ' ' || s[size_s] == '\n' || s[size_s] == '\t')
+	{
+		index_end++;
+		size_s--;
+	}
+	while (i <= index_end)
+	{
+		ret[j] = s[i];
+		i++;
+		j++;
+	}
+	ret[j] = '\0';
+	return (ret);
 
 }
